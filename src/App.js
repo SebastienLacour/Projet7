870ca7a7772a24bfb9fd86a0dbbb38267409e117
SebastienLacour <105979624+SebastienLacour@users.noreply.github.com>
2023-01-11 22:26:42 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import './styles/style.css';
+// Importation de react
 import React from "react";
-import  ReactDOM  from "react-dom";
+
+// Importation du css pour la mise en page
+import './styles/style.css';
+
+// Importation des fonctions routes pouir créer des routes pour notre application
 import {Routes, Route} from "react-router-dom"
+
+// Importation des fonctions useState et useEffect pour faire notre fetch qui va récupérer les données à afficher
+import { useState, useEffect } from 'react';
+
+// Importation des différents composants qui affichera les différentes pages de l'application
 import Home from './components/home';
-import Apropos from './components/aPropos/aPropos';
-import Logement from './components/logement/logement';
-import appartList from './components/apparts';
+import Apropos from './components/aPropos';
+import Logement from './components/logement';
 import NotFound from './components/pageNotFound';
 
 function App() {
+
+  // Créons notre fonction useState et initialisons la à un tableau vide
+  const [data, setData] = useState([])
+
+  // Créons la fonction fetchData qui va récupérer les données de l'api et les afficher dans la variable data
+  const fetchData = () => {
+    fetch('https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        console.log("data", data)
+        setData(json)
+      })
+      .catch((error) => console.log(error))
+  }
+
+  // Appelons la fonction fetchData dans le useEffect pour récupérer le résultat de l'appel à l'api
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log("data", data)
+
+
   return (
+    // Mise en place des routes permettant d'afficher les 4 pages de l'application, en plaçant en props la variable data permettant l'affichage des différents appartements ainsi que leurs informations
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home data ={data} />} />
         <Route path='/a-propos' element={<Apropos />}/>
-        <Route path='/logement/:id' element={<Logement/>}/>
+        <Route path='/logement/:id' element={<Logement data ={data}/>}/>
         <Route path='*' element={<NotFound />}/>
       </Routes>
     </div>
