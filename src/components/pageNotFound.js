9a7f2f15c42d68107493bcfc159732580fd628f4
SebastienLacour <105979624+SebastienLacour@users.noreply.github.com>
2023-01-12 17:58:21 +0100

    Add files via upload

--- src/components/pageNotFound.js
+++ src/components/pageNotFound.js
@@ -1,21 +1,23 @@
-import React from "react";
+// Importation des composant header et footer pour la page d'erreur
 import Header from "./header";
 import Footer from "./footer";
+
+// Importation de link pour créer le lien vers la page d'accueil
 import { Link } from "react-router-dom";
+
+//Importation du style css de la page d'erreur
 import "../styles/pageNotFound.css"
 
-function NotFound() {
+export default function NotFound() {
     return (
         <div>
             <Header />
             <div className="error__main">
-            <h1 className="error__title">404</h1>
-            <h2 className="error__message">Oups! La page que vous demandez n'existe pas.</h2>
-            <Link className="error__link" to="/">Retourner sur la page d'accueil</Link>
+                <h1 className="error__title">404</h1>
+                <h2 className="error__message">Oups! La page que vous demandez n'existe pas.</h2>
+                <Link className="error__link" to="/">Retourner sur la page d'accueil</Link>
             </div>
             <Footer />
         </div>
     )
 }
-
-export default NotFound;
